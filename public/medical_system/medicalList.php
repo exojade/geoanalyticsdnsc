8919805e73703fd7cef6b9fe65d795bd73eb7756
Honey Lou L. Danila <tradebryant@gmail.com>
2024-05-15 15:10:04 +0800

    first commit

--- public/medical_system/medicalList.php
+++ public/medical_system/medicalList.php
@@ -7,14 +7,26 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="AdminLTE_new/dist/css/adminlte.min.css">
 <div class="content-wrapper">
+
+
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>MEDICAL RECORDS MASTERLIST</h1>
+          </div>
+    
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
     <section class="content">
       <div class="container-fluid">
 
         <div class="card">
               <div class="card-header">
-                <h5 class="m-0">MEDICAL RECORDS MASTERLIST
-              
-                </h5>
+           
               </div>
               <div class="card-body table-responsive">
                 <table class="table table-bordered" id="ajax_datatable">
