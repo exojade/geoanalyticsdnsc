d6909e78dc809786676dcad6ef2e234ca6e7457e
Brian Jade Garcia <tradebryant@gmail.com>
2024-11-02 12:59:16 +0800

    this pc

--- public/dashboard_system/dashboard_client.php
+++ public/dashboard_system/dashboard_client.php
@@ -27,6 +27,10 @@
       width: 100%; /* Make the iframe responsive */
       height: 100%;
   }
+
+  div._2p3a {
+    width: auto !important;
+}
 </style>
     <!-- Main content -->
     <section class="content">
@@ -80,14 +84,16 @@
           </div>
           <div class="col-4">
           <div class="iframe-container">
-            <iframe 
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPanaboCityVet&tabs=timeline&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true" 
-                style="border:none; overflow:hidden;" 
-                scrolling="no" 
-                frameborder="0" 
-                allowfullscreen="true" 
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
-            </iframe>
+            <div class="facebook-responsive">
+    <iframe 
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPanaboCityVet&tabs=timeline&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true" 
+        style="border:none; overflow:hidden; width: 100%; height: 100%;" 
+        scrolling="no" 
+        frameborder="0" 
+        allowfullscreen="true" 
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+    </iframe>
+</div>
         </div>
     </div>
     </div>
@@ -112,4 +118,5 @@
 <script src="AdminLTE_new/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
 <script src="AdminLTE_new/plugins/datatables-buttons/js/buttons.print.min.js"></script>
 <script src="AdminLTE_new/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
 <?php require("layouts/footer.php") ?>
